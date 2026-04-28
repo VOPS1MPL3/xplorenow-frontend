c5fa2ebcaa0cf68032bd0805a218302b9c4a2e76
VOPS1MPL3 <fsaracho@uade.edu.ar>
2026-04-27 22:37:11 -0300

    feat: auth - pantalla prueba login

---
+++ app/src/main/java/com/xplorenow/auth/LoginFragment.java
@@ -0,0 +1,30 @@
+package com.xplorenow.auth;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.xplorenow.R;
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class LoginFragment extends Fragment {
+
+    private TextView tvTitulo;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tvTitulo = view.findViewById(R.id.tvTitulo);
+    }
+}
